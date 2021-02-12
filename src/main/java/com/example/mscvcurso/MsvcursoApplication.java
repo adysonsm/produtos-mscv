3d0d287fb096118ff92fb43ae8f383aa4473a953
unknown <adyson.sm@gmail.com>
2021-02-12 02:40:00 -0300

    implementação estados cidades

--- src/main/java/com/example/mscvcurso/MsvcursoApplication.java
+++ src/main/java/com/example/mscvcurso/MsvcursoApplication.java
@@ -1,5 +1,6 @@
 package com.example.mscvcurso;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.mscvcurso.doamin.repositories.CategoriaRepository;
+import com.example.mscvcurso.doamin.repositories.CidadeRepository;
+import com.example.mscvcurso.doamin.repositories.EstadoRepository;
 import com.example.mscvcurso.doamin.repositories.ProdutoRepository;
 import com.example.mscvcurso.domain.Categoria;
+import com.example.mscvcurso.domain.Cidade;
+import com.example.mscvcurso.domain.Estado;
 import com.example.mscvcurso.domain.Produto;
 
 @SpringBootApplication
@@ -19,6 +24,11 @@ public class MsvcursoApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MsvcursoApplication.class, args);
 	}
@@ -39,10 +49,21 @@ public class MsvcursoApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 		
+		Estado est1  = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlandia", est1);
+		Cidade c2 = new Cidade(null, "são Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 		
 		
 		
---
+++ src/main/java/com/example/mscvcurso/doamin/repositories/EstadoRepository.java
@@ -0,0 +1,13 @@
+package com.example.mscvcurso.doamin.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.mscvcurso.domain.Categoria;
+import com.example.mscvcurso.domain.Cidade;
+import com.example.mscvcurso.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
