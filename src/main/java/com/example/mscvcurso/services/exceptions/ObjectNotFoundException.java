63fa4463695069930164c97e43456b4d20cb9230
unknown <adyson.sm@gmail.com>
2021-02-12 02:13:11 -0300

    ajustes no end pont /categoria

---
+++ src/main/java/com/example/mscvcurso/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.example.mscvcurso.services.exceptions;
+
+public class ObjectNotFoundException  extends RuntimeException {
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
