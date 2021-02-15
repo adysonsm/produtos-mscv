0e4f0e26bc02779e41e02679f59040abaf5ca28c
unknown <adyson.sm@gmail.com>
2021-02-15 15:17:38 -0300

    add post categoria

--- src/main/java/com/example/mscvcurso/services/CategoriaService.java
+++ src/main/java/com/example/mscvcurso/services/CategoriaService.java
@@ -13,11 +13,16 @@ import com.example.mscvcurso.services.exceptions.ObjectNotFoundException;
 public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
-		
+
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
-				 "Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName())); 
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
+
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
 	}
 }
