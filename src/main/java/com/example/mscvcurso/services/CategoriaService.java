276bca9aac49208cefa27d9ac3f661427dc1f5cb
unknown <adyson.sm@gmail.com>
2021-02-15 18:10:43 -0300

    pagination categoria

--- src/main/java/com/example/mscvcurso/services/CategoriaService.java
+++ src/main/java/com/example/mscvcurso/services/CategoriaService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.example.mscvcurso.domain.Categoria;
@@ -45,5 +47,10 @@ public class CategoriaService {
 	public List<Categoria> findAll() {
 		return repo.findAll();
 	}
-
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String ordeBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page,linesPerPage,  org.springframework.data.domain.Sort.Direction.valueOf(direction),ordeBy);
+		return repo.findAll(pageRequest);
+	}
+ 
 }
