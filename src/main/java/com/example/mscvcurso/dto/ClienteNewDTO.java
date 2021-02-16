8c4b8df4e3f405519635d7a696fb5ca9a7c1c60b
unknown <adyson.sm@gmail.com>
2021-02-16 16:56:51 -0300

    validation email custom

--- src/main/java/com/example/mscvcurso/dto/ClienteNewDTO.java
+++ src/main/java/com/example/mscvcurso/dto/ClienteNewDTO.java
@@ -7,6 +7,9 @@ import javax.validation.constraints.NotEmpty;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.example.mscvcurso.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 	
 	/**
--- src/main/java/com/example/mscvcurso/services/validation/ClientInsertValidator.java
+++ src/main/java/com/example/mscvcurso/services/validation/ClientInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.example.mscvcurso.domain.Cliente;
 import com.example.mscvcurso.domain.enums.TipoCliente;
 import com.example.mscvcurso.dto.ClienteNewDTO;
+import com.example.mscvcurso.repositories.ClienteRepository;
 import com.example.mscvcurso.resources.exception.FieldMessage;
 import com.example.mscvcurso.services.validation.utils.BR;
 
+
 public class ClientInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +33,11 @@ public class ClientInsertValidator implements ConstraintValidator<ClienteInsert,
 		}
 		if(objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("Cpf ou cnpj ", "cnpj INVÁLIDO"));
+		}
+		
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if(aux!= null) {
+			list.add(new FieldMessage("email", "email já existe"));
 		}
 
 		for (FieldMessage e : list) {
